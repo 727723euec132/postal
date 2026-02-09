fa4c875b11dfd60b041c8412df7254e6a5c97a49
727723euec132 <727723euec132@skcet.ac.in>
2026-02-09 13:07:08 +0530

    Delete backend directory

--- backend/src/routes/weather.js
+++
@@ -1,7 +0,0 @@
-// Routes for weather lookups and impact reporting.
-import express from "express";
-import { getWeatherImpact } from "../controllers/weatherController.js";
-
-export const weatherRouter = express.Router();
-
-weatherRouter.get("/", getWeatherImpact);
--- backend/src/services/mlService.js
+++
@@ -1,8 +0,0 @@
-// Service to call the ML microservice for delay prediction.
-import axios from "axios";
-
-export const requestDelayPrediction = async (features) => {
-  const baseUrl = process.env.ML_SERVICE_URL || "http://localhost:8000";
-  const response = await axios.post(`${baseUrl}/predict`, features);
-  return response.data;
-};
